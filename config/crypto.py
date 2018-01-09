88846505f5326432f2d0efe441a9334453f7196c
Steven Gotthardt <steven.gotthardt@microchip.com>
2018-01-09 15:00:17 -0700

    Initial Commit to get a stable baseline. Generates include file

--- config/crypto.py
+++ config/crypto.py
@@ -1,17 +1,133 @@
 def instantiateComponent(cryptoComponent):
-
-	cryptoHW = cryptoComponent.createBooleanSymbol("hw", None)
+	cryptoHW = cryptoComponent.createBooleanSymbol("cryptoHW", None)
 	cryptoHW.setLabel("Use Hardware Acceleration?")
-	cryptoHW.setDescription("Enables harware acceleration of cryptograhic library")
+	cryptoHW.setDescription("Enables hardware acceleration of cryptographic library")
 
 	if (Peripheral.moduleExists("ICM")):
 		cryptoHW.setVisible(True)
 		cryptoHW.setDefaultValue(True)
 	else:
 		cryptoHW.setVisible(False)
+		cryptoHW.setDefaultValue(False)
+
+	maskFamily = cryptoComponent.createStringSymbol("maskFamily", None)
+	maskFamily.setVisible(False)
+	if "PIC32C" in Variables.get("__PROCESSOR"):
+		maskFamily.setDefaultValue("PIC32C")
+	else:
+		maskFamily.setDefaultValue("PIC32M")
+
+	cryptoMenu = cryptoComponent.createMenuSymbol(None, None)
+	cryptoMenu.setLabel("Crypto Settings")
+
+	# Random Number Generation
+	cryptoRandom = cryptoComponent.createBooleanSymbol("cryptoRandom", cryptoMenu)
+	cryptoRandom.setLabel("TRNG")
+	cryptoRandom.setDescription("Enables True Random Number Generator")
+	cryptoRandom.setDefaultValue(False)
+
+	cryptoCipherAES_CTR = cryptoComponent.createBooleanSymbol("cryptoCipherAES_CTR", cryptoMenu)
+	cryptoCipherAES_CTR.setLabel("AES - CTR")
+	cryptoCipherAES_CTR.setDescription("Enables Advanced Encryption Standard: Counter Mode")
+	cryptoCipherAES_CTR.setDefaultValue(False)
+
+	cryptoCipherAES_CBC = cryptoComponent.createBooleanSymbol("cryptoCipherAES_CBC", cryptoMenu)
+	cryptoCipherAES_CBC.setLabel("AES - CBC")
+	cryptoCipherAES_CBC.setDescription("Enables Advanced Encryption Standard: Cipher Block Chaining")
+	cryptoCipherAES_CBC.setDefaultValue(False)
+
+	cryptoCipherAES_ECB = cryptoComponent.createBooleanSymbol("cryptoCipherAES_ECB", cryptoMenu)
+	cryptoCipherAES_ECB.setLabel("AES - ECB")
+	cryptoCipherAES_ECB.setDescription("Enables Advanced Encryption Standard: Electronic Code Book")
+	cryptoCipherAES_ECB.setDefaultValue(False)
+
+	cryptoCipherAES_GCM = cryptoComponent.createBooleanSymbol("cryptoCipherAES_GCM", cryptoMenu)
+	cryptoCipherAES_GCM.setLabel("AES - GCM")
+	cryptoCipherAES_GCM.setDescription("Enables Advanced Encryption Standard: Galois/Counter Mode")
+	cryptoCipherAES_GCM.setDefaultValue(False)
+
+	# Hashes
+	cryptoHashMD5 = cryptoComponent.createBooleanSymbol("cryptoHashMD5", cryptoMenu)
+	cryptoHashMD5.setLabel("Hash - MD5")
+	cryptoHashMD5.setDescription("Enables Hashing function MD5")
+	cryptoHashMD5.setDefaultValue(False)
+
+	cryptoHashSHA1 = cryptoComponent.createBooleanSymbol("cryptoHashSHA1", cryptoMenu)
+	cryptoHashSHA1.setLabel("Hash - SHA-1")
+	cryptoHashSHA1.setDescription("Enables Hashing function SHA-1")
+	cryptoHashSHA1.setDefaultValue(False)
+
+	cryptoHashSHA256 = cryptoComponent.createBooleanSymbol("cryptoHashSHA256", cryptoMenu)
+	cryptoHashSHA256.setLabel("Hash - SHA-256")
+	cryptoHashSHA256.setDescription("Enables Hashing function SHA-1")
+	cryptoHashSHA256.setDefaultValue(False)
+
+	cryptoHashSHA384 = cryptoComponent.createBooleanSymbol("cryptoHashSHA384", cryptoMenu)
+	cryptoHashSHA384.setLabel("Hash - SHA-384")
+	cryptoHashSHA384.setDescription("Enables Hashing function SHA-384")
+	cryptoHashSHA384.setDefaultValue(False)
+
+	cryptoHashSHA512 = cryptoComponent.createBooleanSymbol("cryptoHashSHA512", cryptoMenu)
+	cryptoHashSHA512.setLabel("Hash - SHA-512")
+	cryptoHashSHA512.setDescription("Enables Hashing function SHA-512")
+	cryptoHashSHA512.setDefaultValue(False)
+
+	cryptoHashHMAC = cryptoComponent.createBooleanSymbol("cryptoHashHMAC", cryptoMenu)
+	cryptoHashHMAC.setLabel("Hash - HMAC")
+	cryptoHashHMAC.setDescription("Enables Keyed-Hash Message Authentication Code")
+	cryptoHashHMAC.setDefaultValue(False)
+
+	cryptoHuffman = cryptoComponent.createBooleanSymbol("cryptoHuffman", cryptoMenu)
+	cryptoHuffman.setLabel("Huffman Coding")
+	cryptoHuffman.setDescription("Enables Huffman Coding Compression")
+	cryptoHuffman.setDefaultValue(False)
+
+	# General Ciphers
+	cryptoCipherECC = cryptoComponent.createBooleanSymbol("cryptoCipherECC", cryptoMenu)
+	cryptoCipherECC.setLabel("Cipher - ECC")
+	cryptoCipherECC.setDescription("Enables Eliptical Curve Cryptograpy")
+	cryptoCipherECC.setDefaultValue(False)
+
+	cryptoCipherRSA = cryptoComponent.createBooleanSymbol("cryptoCipherRSA", cryptoMenu)
+	cryptoCipherRSA.setLabel("Cipher - RSA")
+	cryptoCipherRSA.setDescription("Enables RSA Cryptosystem")
+	cryptoCipherRSA.setDefaultValue(False)
+
+	cryptoCipher3DES = cryptoComponent.createBooleanSymbol("cryptoCipher3DES", cryptoMenu)
+	cryptoCipher3DES.setLabel("Cipher - DES/TDES")
+	cryptoCipher3DES.setDescription("Enables (Triple) DES Cryptosystem")
+	cryptoCipher3DES.setDefaultValue(False)
+
+	# Pass define into compiler so Crypto will read config.h
+	#cryptoHaveConfig = cryptoComponent.createCompilerDefineSymbol("cryptoHaveConfig", None)
+	#cryptoHaveConfig.setDefaultValue("HAVE_CONFIG_H")
+
+	configName = Variables.get("__CONFIGURATION_NAME")
+
+	cryptoHeaderFile = cryptoComponent.createFileSymbol(None, None)
+	cryptoHeaderFile.setSourcePath("templates/crypto_config.h.ftl")
+	cryptoHeaderFile.setOutputName("config.h")
+	cryptoHeaderFile.setDestPath("crypto/")
+	cryptoHeaderFile.setProjectPath("crypto/")
+	cryptoHeaderFile.setMarkup(True)
+	cryptoHeaderFile.setType("HEADER")
+
+#	cryptoSourceFile = cryptoComponent.createFileSymbol(None, None)
+#	cryptoSourceFile.setSourcePath("crypto/templates/crypto_config.c.ftl")
+#	cryptoSourceFile.setOutputName("crypto.c")
+#	cryptoSourceFile.setDestPath("crypto/")
+#	cryptoSourceFile.setProjectPath("crypto/")
+#	cryptoSourceFile.setType("SOURCE")
+
+def cryptoHWLabels(cryptoHW, test):
+	if test.getValue() == True:
+		print("HW Acceleration")
+	else:
+		print("SW Solution")
+
 
 def onDependentComponentAdded(cryptoComponent, id, trngComponent):
-	
 	if id == "LIB_CRYPTO_Dependency":
 		trngComponent.setSymbolValue("TRNG_Reserved", "TRNGReservedSymbol", True, 1)
-
+		trngSymbol = Database.getSymbolByID(trngComponent.getID(), "TRNG_Reserved")
+		trngSymbol.setReadOnly(True)
---
+++ templates/crypto_config.h.ftl
@@ -0,0 +1,121 @@
+<#--
+/*******************************************************************************
+  Cryptographic Library Interface Definition
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    crypto_config.h
+
+  Summary:
+    Cryptographic Library.
+
+  Description:
+    This file contains the interface definition for the Crypto Library.
+    It sets up the definitions required to add or remove elements of the
+    library that the user has specified.
+*******************************************************************************/
+
+/*******************************************************************************
+Copyright (c) 2018 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+*******************************************************************************/
+-->
+/*** Crypto Library Configuration ***/
+
+#define WC_NO_HARDEN
+#define MICROCHIP_MPLAB_HARMONY
+#define HAVE_MCAPI
+<#if maskFamily == "PIC32C">
+  <#lt>#define MICROCHIP_PIC32C
+  <#if cryptoHW == true>
+    <#lt>#define WOLFSSL_MICROCHIP_PIC32C
+    <#if cryptoHashMD5 || cryptoHashSHA1 || cryptoHashSHA256 || cryptoHashSHA384 || cryptoHashSHA512>
+      <#lt>#define WOLFSSL_PIC32C_HASH
+    </#if>
+  </#if>
+<#else>
+  <#lt>#define MICROCHIP_PIC32
+  <#if cryptoHW == true>
+    <#lt>#define WOLFSSL_MICROCHIP_PIC32MZ
+    <#if cryptoHashMD5 || cryptoHashSHA1 || cryptoHashSHA256 || cryptoHashSHA384 || cryptoHashSHA512>
+      <#lt>#define WOLFSSL_PIC32MZ_HASH
+    </#if>
+  </#if>
+</#if>
+
+<#if CONFIG_USE_3RDPARTY_WOLFSSL!false == false>
+    <#lt>#define NO_CERTS
+    <#lt>#define NO_PWDBASED
+    <#lt>#define NO_OLD_TLS
+    <#if cryptoHashMD5 == false && cryptoCipherECC == false>
+        <#lt>#define NO_MD5
+    </#if>
+    <#if cryptoHashSHA1 == false && cryptoCipherECC == false>
+        <#lt>#define NO_SHA
+    </#if>
+    <#if cryptoCipherAES_CTR == false && cryptoCipherAES_CBC == false && cryptoCipherAES_ECB == false && cryptoCipherAES_GCM == false>
+        <#lt>#define NO_AES
+    </#if>
+    <#if cryptoHashSHA256 == false>
+        <#lt>#define NO_SHA256
+    </#if>
+    <#if cryptoHashSHA384 == true>
+        <#lt>#define WOLFSSL_SHA384
+    </#if>
+    <#if cryptoHashSHA512 == true || cryptoHashSHA384 == true>
+        <#lt>#define WOLFSSL_SHA512
+    </#if>
+    <#if cryptoCipherECC == true>
+        <#lt>#define HAVE_ECC
+    </#if>
+    <#if cryptoCipherECC == false && cryptoCipherRSA == false>
+        <#lt>#define NO_ASN
+    <#else>
+        <#lt>#define USE_FAST_MATH
+    </#if>
+    <#if cryptoHuffman == true>
+        <#lt>#define HAVE_LIBZ
+    </#if>
+    <#if cryptoCipherRSA == false>
+        <#lt>#define NO_RSA
+    </#if>
+    <#if cryptoHashHMAC == false && cryptoCipherECC == false>
+        <#lt>#define NO_HMAC
+    </#if>
+</#if>
+<#if cryptoCipherAES_CTR == true>
+#define WOLFSSL_AES_COUNTER
+</#if>
+<#if cryptoRandom == true>
+#define WOLFSSL_AES_DIRECT
+</#if>
+<#if cryptoRandom == false && cryptoCipherECC == false && cryptoCipherRSA == false>
+#define NO_RNG
+</#if>
+
+<#--
+/*******************************************************************************
+ End of File
+*/
+-->
+
