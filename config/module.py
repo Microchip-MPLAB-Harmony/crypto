3f63fd0eea7aebc3302c851ec0196243893e0da4
Ryan Rogerson <ryan.rogerson@microchip.com>
2018-02-12 13:42:23 -0700

    addCapability API change

--- config/module.py
+++ config/module.py
@@ -2,7 +2,8 @@ def loadModule():
     cryptoComponent = Module.CreateComponent("lib_crypto",
                                              "Cryptographic (Crypto) Library",
                                              "/Libraries/", "config/crypto.py")
-    cryptoComponent.addCapability("LIB_CRYPTO")
+    
+    cryptoComponent.addCapability("LIB_CRYPTO", "LIB_CRYPTO")
 
     if Peripheral.moduleExists("TRNG"):
         cryptoComponent.addDependency("LIB_CRYPTO_Dependency", "TRNG")
