0c9354bbe84581a01d36809a0682d5c40c3b2170
Peter Wright <peter.wright@microchip.com>
2018-11-16 13:32:08 -0700

    Crypto Help Documentation Output.

---
+++ doc/HTML/harmony_help_map.h
@@ -0,0 +1,95 @@
+#define IDH_Release_Information_for_Crypto                 0x00000000 // Release Information for Crypto
+#define IDH_Release_Notes                                  0x00000001 // Release Notes
+#define IDH_Applications                                   0x00000002 // Applications
+#define IDH_CRYPTO_large_hash_DRV_Usage_Model_2            0x00000003 // CRYPTO_large_hash DRV Usage Model 2
+#define IDH_Building_the_Application                       0x00000004 // Building the Application
+#define IDH_Configuring_the_Hardware                       0x00000005 // Configuring the Hardware
+#define IDH_Running_the_Demonstration                      0x00000006 // Running the Demonstration
+#define IDH_CRYPTO_encrypt_decrypt_Library                 0x00000007 // CRYPTO_encrypt_decrypt Library
+#define IDH_Building_the_Application_ED                    0x00000008 // Building the Application ED
+#define IDH_Configuring_the_Hardware_ED                    0x00000009 // Configuring the Hardware ED
+#define IDH_Running_the_Demostration                       0x0000000A // Running the Demostration
+#define IDH_Cryptographic__Crypto__Library                 0x0000000B // Cryptographic (Crypto) Library
+#define IDH_CRYPTO_Introduction                            0x0000000C // CRYPTO Introduction
+#define IDH_CRYPTO_Using_the_Library                       0x0000000D // CRYPTO Using the Library
+#define IDH_CRYPTO_Abstraction_Model                       0x0000000E // CRYPTO Abstraction Model
+#define IDH_CRYPTO_Library_Overview                        0x0000000F // CRYPTO Library Overview
+#define IDH_CRYPTO_Configuring_the_Library                 0x00000010 // CRYPTO Configuring the Library
+#define IDH_CRYPTO_Building_the_Library                    0x00000011 // CRYPTO Building the Library
+#define IDH_CRYPTO_Library_Interface                       0x00000012 // CRYPTO Library Interface
+#define IDH_CRYPT_ERROR_StringGet_int_char_                0x00000013 // CRYPT_ERROR_StringGet@int@char*
+#define IDH_CRYPT_HUFFMAN_Compress_unsigned_char__unsigned_int_unsigned_char__unsigned_int_unsigned_i 0x00000014 // CRYPT_HUFFMAN_Compress@unsigned char*@unsigned int@unsigned char*@unsigned int@unsigned int
+#define IDH_CRYPT_HUFFMAN_DeCompress_unsigned_char__unsigned_int_unsigned_char__unsigned_int 0x00000015 // CRYPT_HUFFMAN_DeCompress@unsigned char*@unsigned int@unsigned char*@unsigned int
+#define IDH_CRYPT_MD5_DataAdd_CRYPT_MD5_CTX__unsigned_char__unsigned_int 0x00000016 // CRYPT_MD5_DataAdd@CRYPT_MD5_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_MD5_Finalize_CRYPT_MD5_CTX__unsigned_char_ 0x00000017 // CRYPT_MD5_Finalize@CRYPT_MD5_CTX*@unsigned char*
+#define IDH_CRYPT_MD5_Initialize_CRYPT_MD5_CTX_            0x00000018 // CRYPT_MD5_Initialize@CRYPT_MD5_CTX*
+#define IDH_CRYPT_MD5_DataSizeSet_CRYPT_MD5_CTX__unsigned_int 0x00000019 // CRYPT_MD5_DataSizeSet@CRYPT_MD5_CTX*@unsigned int
+#define IDH_CRYPT_RNG_Initialize                           0x0000001A // CRYPT_RNG_Initialize
+#define IDH_CRYPT_RNG_BlockGenerate_CRYPT_RNG_CTX__unsigned_char__unsigned_int 0x0000001B // CRYPT_RNG_BlockGenerate@CRYPT_RNG_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_RNG_Get_CRYPT_RNG_CTX__unsigned_char_    0x0000001C // CRYPT_RNG_Get@CRYPT_RNG_CTX*@unsigned char*
+#define IDH_CRYPT_AES_CBC_Decrypt_CRYPT_AES_CTX__unsigned_char__unsigned_char__unsigned_int 0x0000001D // CRYPT_AES_CBC_Decrypt@CRYPT_AES_CTX*@unsigned char*@unsigned char*@unsigned int
+#define IDH_CRYPT_AES_CBC_Encrypt_CRYPT_AES_CTX__unsigned_char__unsigned_char__unsigned_int 0x0000001E // CRYPT_AES_CBC_Encrypt@CRYPT_AES_CTX*@unsigned char*@unsigned char*@unsigned int
+#define IDH_CRYPT_AES_CTR_Encrypt_CRYPT_AES_CTX__unsigned_char__unsigned_char__unsigned_int 0x0000001F // CRYPT_AES_CTR_Encrypt@CRYPT_AES_CTX*@unsigned char*@unsigned char*@unsigned int
+#define IDH_CRYPT_AES_DIRECT_Decrypt_CRYPT_AES_CTX__unsigned_char__unsigned_char_ 0x00000020 // CRYPT_AES_DIRECT_Decrypt@CRYPT_AES_CTX*@unsigned char*@unsigned char*
+#define IDH_CRYPT_AES_DIRECT_Encrypt_CRYPT_AES_CTX__unsigned_char__unsigned_char_ 0x00000021 // CRYPT_AES_DIRECT_Encrypt@CRYPT_AES_CTX*@unsigned char*@unsigned char*
+#define IDH_CRYPT_AES_IvSet_CRYPT_AES_CTX__unsigned_char_  0x00000022 // CRYPT_AES_IvSet@CRYPT_AES_CTX*@unsigned char*
+#define IDH_CRYPT_AES_KeySet_CRYPT_AES_CTX__unsigned_char__unsigned_int_unsigned_char__int 0x00000023 // CRYPT_AES_KeySet@CRYPT_AES_CTX*@unsigned char*@unsigned int@unsigned char*@int
+#define IDH_CRYPT_ECC_DHE_KeyMake_CRYPT_ECC_CTX__CRYPT_RNG_CTX__int 0x00000024 // CRYPT_ECC_DHE_KeyMake@CRYPT_ECC_CTX*@CRYPT_RNG_CTX*@int
+#define IDH_CRYPT_ECC_DHE_SharedSecretMake_CRYPT_ECC_CTX__CRYPT_ECC_CTX__unsigned_char__unsigned_int_ 0x00000025 // CRYPT_ECC_DHE_SharedSecretMake@CRYPT_ECC_CTX*@CRYPT_ECC_CTX*@unsigned char*@unsigned int@unsigned int*
+#define IDH_CRYPT_ECC_DSA_HashSign_CRYPT_ECC_CTX__CRYPT_RNG_CTX__unsigned_char__unsigned_int_unsigned 0x00000026 // CRYPT_ECC_DSA_HashSign@CRYPT_ECC_CTX*@CRYPT_RNG_CTX*@unsigned char*@unsigned int@unsigned int*@unsigned char*@unsigned int
+#define IDH_CRYPT_ECC_DSA_HashVerify_CRYPT_ECC_CTX__unsigned_char__unsigned_int_unsigned_char__unsign 0x00000027 // CRYPT_ECC_DSA_HashVerify@CRYPT_ECC_CTX*@unsigned char*@unsigned int@unsigned char*@unsigned int@int*
+#define IDH_CRYPT_ECC_Free_CRYPT_ECC_CTX_                  0x00000028 // CRYPT_ECC_Free@CRYPT_ECC_CTX*
+#define IDH_CRYPT_ECC_Initialize_CRYPT_ECC_CTX_            0x00000029 // CRYPT_ECC_Initialize@CRYPT_ECC_CTX*
+#define IDH_CRYPT_ECC_KeySizeGet_CRYPT_ECC_CTX_            0x0000002A // CRYPT_ECC_KeySizeGet@CRYPT_ECC_CTX*
+#define IDH_CRYPT_ECC_PrivateImport_CRYPT_ECC_CTX__unsigned_char__unsigned_int_unsigned_char__unsigne 0x0000002B // CRYPT_ECC_PrivateImport@CRYPT_ECC_CTX*@unsigned char*@unsigned int@unsigned char*@unsigned int
+#define IDH_CRYPT_ECC_PublicExport_CRYPT_ECC_CTX__unsigned_char__unsigned_int_unsigned_int_ 0x0000002C // CRYPT_ECC_PublicExport@CRYPT_ECC_CTX*@unsigned char*@unsigned int@unsigned int*
+#define IDH_CRYPT_ECC_PublicImport_CRYPT_ECC_CTX__unsigned_char__unsigned_int 0x0000002D // CRYPT_ECC_PublicImport@CRYPT_ECC_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_ECC_SignatureSizeGet_CRYPT_ECC_CTX_      0x0000002E // CRYPT_ECC_SignatureSizeGet@CRYPT_ECC_CTX*
+#define IDH_CRYPT_RSA_EncryptSizeGet_CRYPT_RSA_CTX_        0x0000002F // CRYPT_RSA_EncryptSizeGet@CRYPT_RSA_CTX*
+#define IDH_CRYPT_RSA_Free_CRYPT_RSA_CTX_                  0x00000030 // CRYPT_RSA_Free@CRYPT_RSA_CTX*
+#define IDH_CRYPT_RSA_Initialize_CRYPT_RSA_CTX_            0x00000031 // CRYPT_RSA_Initialize@CRYPT_RSA_CTX*
+#define IDH_CRYPT_RSA_PrivateDecrypt_CRYPT_RSA_CTX__unsigned_char__unsigned_int_unsigned_char__unsign 0x00000032 // CRYPT_RSA_PrivateDecrypt@CRYPT_RSA_CTX*@unsigned char*@unsigned int@unsigned char*@unsigned int
+#define IDH_CRYPT_RSA_PrivateKeyDecode_CRYPT_RSA_CTX__unsigned_char__unsigned_int 0x00000033 // CRYPT_RSA_PrivateKeyDecode@CRYPT_RSA_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_RSA_PublicEncrypt_CRYPT_RSA_CTX__unsigned_char__unsigned_int_unsigned_char__unsigne 0x00000034 // CRYPT_RSA_PublicEncrypt@CRYPT_RSA_CTX*@unsigned char*@unsigned int@unsigned char*@unsigned int@CRYPT_RNG_CTX*
+#define IDH_CRYPT_RSA_PublicKeyDecode_CRYPT_RSA_CTX__unsigned_char__unsigned_int 0x00000035 // CRYPT_RSA_PublicKeyDecode@CRYPT_RSA_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_TDES_CBC_Decrypt_CRYPT_TDES_CTX__unsigned_char__unsigned_char__unsigned_int 0x00000036 // CRYPT_TDES_CBC_Decrypt@CRYPT_TDES_CTX*@unsigned char*@unsigned char*@unsigned int
+#define IDH_CRYPT_TDES_CBC_Encrypt_CRYPT_TDES_CTX__unsigned_char__unsigned_char__unsigned_int 0x00000037 // CRYPT_TDES_CBC_Encrypt@CRYPT_TDES_CTX*@unsigned char*@unsigned char*@unsigned int
+#define IDH_CRYPT_TDES_IvSet_CRYPT_TDES_CTX__unsigned_char_ 0x00000038 // CRYPT_TDES_IvSet@CRYPT_TDES_CTX*@unsigned char*
+#define IDH_CRYPT_TDES_KeySet_CRYPT_TDES_CTX__unsigned_char__unsigned_char__int 0x00000039 // CRYPT_TDES_KeySet@CRYPT_TDES_CTX*@unsigned char*@unsigned char*@int
+#define IDH_CRYPT_HMAC_DataAdd_CRYPT_HMAC_CTX__unsigned_char__unsigned_int 0x0000003A // CRYPT_HMAC_DataAdd@CRYPT_HMAC_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_HMAC_Finalize_CRYPT_HMAC_CTX__unsigned_char_ 0x0000003B // CRYPT_HMAC_Finalize@CRYPT_HMAC_CTX*@unsigned char*
+#define IDH_CRYPT_HMAC_SetKey_CRYPT_HMAC_CTX__int_unsigned_char__unsigned_int 0x0000003C // CRYPT_HMAC_SetKey@CRYPT_HMAC_CTX*@int@unsigned char*@unsigned int
+#define IDH_CRYPT_SHA_DataAdd_CRYPT_SHA_CTX__unsigned_char__unsigned_int 0x0000003D // CRYPT_SHA_DataAdd@CRYPT_SHA_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_SHA_Finalize_CRYPT_SHA_CTX__unsigned_char_ 0x0000003E // CRYPT_SHA_Finalize@CRYPT_SHA_CTX*@unsigned char*
+#define IDH_CRYPT_SHA_Initialize_CRYPT_SHA_CTX_            0x0000003F // CRYPT_SHA_Initialize@CRYPT_SHA_CTX*
+#define IDH_CRYPT_SHA_DataSizeSet_CRYPT_SHA_CTX__unsigned_int 0x00000040 // CRYPT_SHA_DataSizeSet@CRYPT_SHA_CTX*@unsigned int
+#define IDH_CRYPT_SHA256_DataAdd_CRYPT_SHA256_CTX__unsigned_char__unsigned_int 0x00000041 // CRYPT_SHA256_DataAdd@CRYPT_SHA256_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_SHA256_Finalize_CRYPT_SHA256_CTX__unsigned_char_ 0x00000042 // CRYPT_SHA256_Finalize@CRYPT_SHA256_CTX*@unsigned char*
+#define IDH_CRYPT_SHA256_Initialize_CRYPT_SHA256_CTX_      0x00000043 // CRYPT_SHA256_Initialize@CRYPT_SHA256_CTX*
+#define IDH_CRYPT_SHA256_DataSizeSet_CRYPT_SHA256_CTX__unsigned_int 0x00000044 // CRYPT_SHA256_DataSizeSet@CRYPT_SHA256_CTX*@unsigned int
+#define IDH_CRYPT_SHA384_DataAdd_CRYPT_SHA384_CTX__unsigned_char__unsigned_int 0x00000045 // CRYPT_SHA384_DataAdd@CRYPT_SHA384_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_SHA384_Finalize_CRYPT_SHA384_CTX__unsigned_char_ 0x00000046 // CRYPT_SHA384_Finalize@CRYPT_SHA384_CTX*@unsigned char*
+#define IDH_CRYPT_SHA384_Initialize_CRYPT_SHA384_CTX_      0x00000047 // CRYPT_SHA384_Initialize@CRYPT_SHA384_CTX*
+#define IDH_CRYPT_SHA512_DataAdd_CRYPT_SHA512_CTX__unsigned_char__unsigned_int 0x00000048 // CRYPT_SHA512_DataAdd@CRYPT_SHA512_CTX*@unsigned char*@unsigned int
+#define IDH_CRYPT_SHA512_Finalize_CRYPT_SHA512_CTX__unsigned_char_ 0x00000049 // CRYPT_SHA512_Finalize@CRYPT_SHA512_CTX*@unsigned char*
+#define IDH_CRYPT_SHA512_Initialize_CRYPT_SHA512_CTX_      0x0000004A // CRYPT_SHA512_Initialize@CRYPT_SHA512_CTX*
+#define IDH_CRYPT_AES_CTX                                  0x0000004B // CRYPT_AES_CTX
+#define IDH_CRYPT_ECC_CTX                                  0x0000004C // CRYPT_ECC_CTX
+#define IDH_CRYPT_HMAC_CTX                                 0x0000004D // CRYPT_HMAC_CTX
+#define IDH_CRYPT_MD5_CTX                                  0x0000004E // CRYPT_MD5_CTX
+#define IDH_CRYPT_RNG_CTX                                  0x0000004F // CRYPT_RNG_CTX
+#define IDH_CRYPT_RSA_CTX                                  0x00000050 // CRYPT_RSA_CTX
+#define IDH_CRYPT_SHA_CTX                                  0x00000051 // CRYPT_SHA_CTX
+#define IDH___MEMBEROVERVIEW_CRYPT_SHA_CTX                 0x00000052 // !!MEMBEROVERVIEW_CRYPT_SHA_CTX
+#define IDH___MEMBERTYPE_Methods_CRYPT_SHA_CTX             0x00000053 // !!MEMBERTYPE_Methods_CRYPT_SHA_CTX
+#define IDH_CRYPT_SHA_CTX____attribute___aligned_128       0x00000054 // CRYPT_SHA_CTX::__attribute__@aligned 128
+#define IDH_CRYPT_SHA256_CTX                               0x00000055 // CRYPT_SHA256_CTX
+#define IDH___MEMBEROVERVIEW_CRYPT_SHA256_CTX              0x00000056 // !!MEMBEROVERVIEW_CRYPT_SHA256_CTX
+#define IDH___MEMBERTYPE_Methods_CRYPT_SHA256_CTX          0x00000057 // !!MEMBERTYPE_Methods_CRYPT_SHA256_CTX
+#define IDH_CRYPT_SHA256_CTX____attribute___aligned_128    0x00000058 // CRYPT_SHA256_CTX::__attribute__@aligned 128
+#define IDH_CRYPT_SHA384_CTX                               0x00000059 // CRYPT_SHA384_CTX
+#define IDH_CRYPT_SHA512_CTX                               0x0000005A // CRYPT_SHA512_CTX
+#define IDH_CRYPT_TDES_CTX                                 0x0000005B // CRYPT_TDES_CTX
+#define IDH_MC_CRYPTO_API_H                                0x0000005C // MC_CRYPTO_API_H
+#define IDH_CRYPTO_Files                                   0x0000005D // CRYPTO Files
+#define IDH_crypto_h                                       0x0000005E // crypto.h
