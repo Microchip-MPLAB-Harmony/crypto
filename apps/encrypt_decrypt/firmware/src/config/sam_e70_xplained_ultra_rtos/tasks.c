b4c4c389e751e0f6ccdda9673629be08394985a3
Peter Wright <peter.wright@microchip.com>
2019-05-10 10:40:19 -0700

    MH3-12173: SAME70 Encrypt Decrypt RTOS Demo Regeneration.

--- apps/encrypt_decrypt/firmware/src/config/sam_e70_xplained_ultra_rtos/tasks.c
+++ apps/encrypt_decrypt/firmware/src/config/sam_e70_xplained_ultra_rtos/tasks.c
@@ -101,7 +101,7 @@ void SYS_Tasks ( void )
         /* Create OS Thread for APP_Tasks. */
     xTaskCreate((TaskFunction_t) _APP_Tasks,
                 "APP_Tasks",
-                8192,
+                6144,
                 NULL,
                 1,
                 &xAPP_Tasks);
